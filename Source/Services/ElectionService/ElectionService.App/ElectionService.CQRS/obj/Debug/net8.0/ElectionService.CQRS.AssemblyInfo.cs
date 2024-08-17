f4024bc61e1a4b993b947939aefb2e11cae61fec
M B A R K <mbark1236@gmail.com>
2024-08-17 20:21:43 +0100

    Feat (Gateway): Add the AuthService Route and Cluster

--- Source/Services/ElectionService/ElectionService.App/ElectionService.CQRS/obj/Debug/net8.0/ElectionService.CQRS.AssemblyInfo.cs
+++ Source/Services/ElectionService/ElectionService.App/ElectionService.CQRS/obj/Debug/net8.0/ElectionService.CQRS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ElectionService.CQRS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8502e628e017e92c7f3f2825c633d165409b822d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3dd0bab06c849fcdac2005e79ce75f0260b41f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ElectionService.CQRS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ElectionService.CQRS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
